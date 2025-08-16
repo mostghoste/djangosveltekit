ac926aef5b37af852a208e0d919b0d93a311acd1
mostghoste <sarunas.jacikas@gmail.com>
2025-08-16 17:49:05 +0300

    aaaa

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,4 +4,5 @@ set -e
 python manage.py collectstatic --noinput || true
 python manage.py migrate --noinput
 
-exec gunicorn core.wsgi:application --bind 0.0.0.0:${BACKEND_PORT:-8081}
+# Container's internal port (default 8000)
+exec gunicorn core.wsgi:application --bind 0.0.0.0:${BACKEND_PORT:-8000}
