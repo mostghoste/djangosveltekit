d3a7340f0947e65cbbfc88d1aa1239c805efcca2
mostghoste <sarunas.jacikas@gmail.com>
2025-08-16 16:51:24 +0300

    Change backend port

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 python manage.py collectstatic --noinput || true
 python manage.py migrate --noinput
 
-exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn core.wsgi:application --bind 0.0.0.0:${BACKEND_PORT:-8081}
