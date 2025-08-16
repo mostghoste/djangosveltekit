15f64bd57a7cbb51586bbde5c2435b23d9ce81ea
mostghoste <sarunas.jacikas@gmail.com>
2025-08-16 18:46:49 +0300

    Add csrf  trusted origins

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -16,6 +16,14 @@ CORS_ALLOWED_ORIGINS = [
     ).split(',')
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    o.strip() for o in os.environ.get(
+        'CSRF_TRUSTED_ORIGINS',
+        'http://localhost:3000'
+    ).split(',') if o.strip()
+]
+
+
 INSTALLED_APPS = [
     'corsheaders',
     'django.contrib.admin',
